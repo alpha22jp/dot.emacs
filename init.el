51ec9cf91beceb9f1dbc8f586a36d2af6df9e4d5
alpha22jp <alpha22jp@gmail.com>
2018-05-02 12:38:01 +0900

    rtags周りの設定を見直し

--- init.el
+++ init.el
@@ -183,7 +183,7 @@
   :diminish irony-mode
   :config
   (setq irony-additional-clang-options '("-std=c++11"))
-  (add-to-list 'company-backends 'company-irony)
+  (push 'company-irony company-backends)
   (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
   (add-hook 'c-mode-hook 'irony-mode)
   (add-hook 'c++-mode-hook 'irony-mode))
@@ -224,16 +224,18 @@
 ;; rtags
 ;;
 (use-package rtags
-  :disabled (eq system-type 'windows-nt)
   :config
-  (when (require 'rtags-helm nil 'noerror)
-    (setq rtags-use-helm t))
+  (setq rtags-autostart-diagnostics t)
+  (rtags-diagnostics)
+  (setq rtags-completions-enabled t)
+  (setq rtags-display-result-backend 'helm)
+  (push 'company-rtags company-backends)
   (add-hook 'c-mode-common-hook
             (lambda ()
               (when (rtags-is-indexed)
                 (local-set-key (kbd "M-.") 'rtags-find-symbol-at-point)
                 (local-set-key (kbd "M-[") 'rtags-find-symbol)
-                (local-set-key (kbd "M-@") 'rtags-find-references)
+                (local-set-key (kbd "M-@") 'rtags-find-references-at-point)
                 (local-set-key (kbd "M-,") 'rtags-location-stack-back)))))
 
 ;; fuzzy-format
