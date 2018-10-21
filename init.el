a246cc5ef817d2cd766797233f1c0617c4bc8696
alpha22jp <alpha22jp@gmail.com>
2018-10-21 10:56:41 +0900

    exec-path-from-shellがWindowsではエラーになるので使わないように変更

--- init.el
+++ init.el
@@ -119,8 +119,9 @@
 
 ;; exec-path-from-shell
 ;;
-(use-package exec-path-from-shell
-  :config (exec-path-from-shell-initialize))
+(unless (eq system-type 'windows-nt)
+  (use-package exec-path-from-shell
+    :config (exec-path-from-shell-initialize)))
 
 ;; recentf
 ;;
